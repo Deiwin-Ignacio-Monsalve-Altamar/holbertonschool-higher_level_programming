a0423addd2cd00c88ce6c219a8ac36065089c6b9
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-04-27 14:18:06 -0500

    tasks 6

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-str1 = "Holberton"
-str2 = "School"
-str1 = str1 + " " + str2
-print("Welcome to {}!".format(str1))
+number = 98
+print("{:d} {}".format(number, "Battery street"))
