b0d31029663102bb0701a65a358b7e0a4a0950cb
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-04-30 11:41:46 -0500

    program that imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+from add_0 import add
+
+def add_numbers():
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
+if __name__ == '__main__':
+    add_numbers()
