dcbbfc32ddfb2803104bb5e721d495d2d5cef323
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-04-30 13:36:08 -0500

    program that imports the variable a from the file variable_load_5.py and prints its value

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+
+def directo():
+    import hidden_4
+    for name in dir(hidden_4):
+        if name[0] != '_' and name[1] != '_':
+            print(name)
+
+
+if __name__ == '__main__':
+    directo()
