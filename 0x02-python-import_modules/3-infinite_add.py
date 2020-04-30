cf2953391110dd8036cb5e22ad7a1b6c99ca2d0f
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-04-30 12:43:07 -0500

    program that prints the result of the addition of all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+
+def addfinite(argv):
+    num = len(argv) - 1
+    if num <= 0:
+        print("{}".format(num))
+    else:
+        add = 0
+        for i in argv[1:]:
+            add = add + int(i)
+        print("{}".format(add))
+
+
+if __name__ == '__main__':
+    import sys
+    addfinite(sys.argv)
