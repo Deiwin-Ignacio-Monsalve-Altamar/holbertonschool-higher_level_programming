b147258d0761ef1066851853644421ab9564d70d
Deiwin-Ignacio-Monsalve-Altamar <1651@hobertonschool.com>
2020-05-05 18:59:56 +0000

    función que encuentre el mayor número entero de una lista

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if len(my_list) == 0:
-        return None
+        return "None"
     max = my_list[0]
     for i in my_list:
-        if i > max
+        if i > max:
             max = i
     return max
