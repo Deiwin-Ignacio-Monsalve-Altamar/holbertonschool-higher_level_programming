54702a45adcfdf49e328300a374af6451b255520
Deiwin-Ignacio-Monsalve-Altamar <1651@hobertonschool.com>
2020-05-05 16:29:19 +0000

    function that prints a matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        count = 0
+        for j in i:
+            count = count + 1
+            if count < len(i):
+                print("{}".format(j), end=" ")
+            else:
+                print("{}".format(j), end="")
+        print()
