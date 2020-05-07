d91fde2834fc6b8f44b1b92d09b77f6a9636466a
Deiwin-Ignacio-Monsalve-Altamar <1651@hobertonschool.com>
2020-05-07 21:48:26 +0000

    tasks advaced

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=[]):
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    
+    sum, div = 0, 0
+    for num in my_list:
+        sum += num[0] * num[1]
+        div += num[1]
+    return sum / div
