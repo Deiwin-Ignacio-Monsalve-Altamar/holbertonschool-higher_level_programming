99081e1c13e76823e2892b7d3a8316d105ca826b
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-05-18 17:07:32 +0000

    Write a function that prints x elements of a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,10 +4,9 @@ def safe_print_list(my_list=[], x=0):
     for index, num in enumerate(my_list):
         try:
             if x > index:
-                print(num, end ="")
+                print("{:d}".format(num), end="")
                 counter += 1
         except (ValueError, TypeError):
             pass
     print()
     return counter
-
