2df967070eaf19068035043b142478602a0c60c7
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-05-19 21:04:16 +0000

    class Square that defines a square by: (based on 5-square.py)

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -67,7 +67,8 @@ class Square:
         """
         if self.__size == 0:
             print()
-        for i in range(self.position[1]):
-            print()
-        for i in range(self.__size):
-            print(" " * self.__position[0] + "#" * self.__size)
+        else:
+            for i in range(self.position[1]):
+                print()
+            for i in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
