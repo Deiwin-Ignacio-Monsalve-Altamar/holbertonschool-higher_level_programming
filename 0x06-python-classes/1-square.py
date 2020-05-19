3f4cdbae52eb8c64ab05d0f7caa5426b8d2f3489
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-05-19 17:45:44 +0000

    class Square that defines a square by: (based on 0-square.py)

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+
+
+"1-square.py class square defines a square"
+
+
 class Square:
+    """Class square"""
     def __init__(self, size):
+        """Inizialition of variables
+        Arg self identificador
+        size tamañe of square"""
         self.__size = size
