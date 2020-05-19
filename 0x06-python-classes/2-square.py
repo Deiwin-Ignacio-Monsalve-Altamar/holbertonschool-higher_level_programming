905ba8fd1993a82f6300cb6f312fba0ef4dce0c0
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-05-19 17:45:20 +0000

    class Square that defines a square by: (based on 1-square.py)

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+
+
+"2-square.py class square defines a square"
+
+
 class Square:
+    """Class square"""
     def __init__(self, size=0):
+        """Inizialition of variables
+        Arg self identificador
+            size tamañe of square"""
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
