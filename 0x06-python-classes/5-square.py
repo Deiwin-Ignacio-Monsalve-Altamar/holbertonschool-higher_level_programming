3435b892e61ee80ffed7c50718c71d41f3f99e40
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-05-19 17:46:37 +0000

    una clase Squareque defina un cuadrado por: (basado en 4-square.py)

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+
+
+"5-square.py class square defines a square"
+
+
 class Square:
+    """Class square"""
     def __init__(self, size=0):
+        """Inizialition of variables
+        Arg self identificador
+            size tamañe of square"""
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -10,9 +19,11 @@ class Square:
 
     @property
     def size(self):
-        return self.__sizes
+        """ Returns size"""
+        return self.__size
 
     @size.setter
+    """Set in the self.__size with value"""
     def size(self, value):
         if type(value) is not int:
             raise TypeError("size must be an integer")
@@ -22,9 +33,11 @@ class Square:
             self.__size = value
 
     def area(self):
+        """ Returns area the square"""
         return self.__size * self.__size
 
     def my_print(self):
+        """Print square # """
         if self.__size == 0:
             print(end="")
         for i in range(self.__size):
