ad840f72d47f6905e958e457f2166a993d16d5e9
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-05-23 03:48:39 +0000

    function that adds 2 integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -13,6 +13,10 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
+    if type(a) is float('inf') or type(b) is float('inf'):
+        raise OverflowError("cannot convert float infinity to integer")
+    if type(a) is float('NaN') or type(b) is float('NaN'):
+        raise OverflowError("cannot convert float infinity to integer")
     if type(b) is float:
         b = int(b)
     if type(a) is float:
