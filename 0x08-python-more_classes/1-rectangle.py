df8f759478ceaf39860624b33bceb3615e6a602c
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-05-25 23:22:18 +0000

    fix tasks

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -27,7 +27,7 @@ class Rectangle:
     def width(self, value):
         """return property
         """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
