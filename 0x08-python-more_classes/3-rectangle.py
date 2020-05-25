ffd02c7951e010a5050845f27c5a1948d5a96a2a
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-05-25 23:28:36 +0000

    fix tasks

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -52,6 +52,7 @@ class Rectangle:
         new_string = ''
         if self.__width == 0 or self.__heigth == 0:
             return new_string
-        for width in range(self.__heigth):
+        for width in range(self.__heigth - 1):
             new_string += ("#" * self.__width) + '\n'
+        new_string += ("#" * self.__width)
         return new_string
