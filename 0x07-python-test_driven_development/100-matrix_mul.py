17e9494c544b0b608b1c9829eb786825b5fff8b6
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-05-25 04:13:38 +0000

    fix tasks

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,35 +1,22 @@
 #!/usr/bin/python3
 """[summary]"""
- 
- 
+
+
 def matrix_mul(m_a, m_b):
     """[summary]
- 
+
     Arguments:
         m_a {[type]} -- [description]
         m_b {[type]} -- [description]
- 
-    Raises:
-        TypeError: [description]
-        TypeError: [description]
-        TypeError: [description]
-        TypeError: [description]
-        TypeError: [description]
-        TypeError: [description]
-        ValueError: [description]
-        ValueError: [description]
-        TypeError: [description]
-        TypeError: [description]
-        ValueError: [description]
     """
     m_c = []
     m_temp = []
     if type(m_a) is not (list):
         raise TypeError("m_a must be a list")
- 
+
     if type(m_b) is not (list):
         raise TypeError("m_b must be a list")
- 
+
     for row in m_a:
         if type(row) is not (list):
             raise TypeError("m_a must be a list of lists")
@@ -38,7 +25,7 @@ def matrix_mul(m_a, m_b):
         for elem in row:
             if type(elem) not in (int, float):
                 raise TypeError("m_a should contain only integers or floats")
- 
+
     for row in m_b:
         if type(row) is not (list):
             raise TypeError("m_b must be a list of lists")
@@ -47,22 +34,22 @@ def matrix_mul(m_a, m_b):
         for elem in row:
             if type(elem) not in (int, float):
                 raise TypeError("m_b should contain only integers or floats")
- 
+
     if len(m_a) is 0 or m_a is None:
         raise ValueError("m_a can't be empty")
- 
+
     if len(m_b) is 0 or m_b is None:
         raise ValueError("m_b can't be empty")
- 
+
     if len(set([len(row) for row in m_a])) is not 1:
         raise TypeError("each row of m_a must be of the same size")
- 
+
     if len(set([len(row) for row in m_b])) is not 1:
         raise TypeError("each row of m_b must be of the same size")
- 
+
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
- 
+
     for d in range(len(m_a)):
         m_temp = []
         for g in range(len(m_b[0])):
