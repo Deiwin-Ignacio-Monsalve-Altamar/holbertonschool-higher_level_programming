37468fe887b26148e33de88ac52b21404fbd6c87
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-06-02 18:27:12 +0000

    class BaseGeometry (based on 5-base_geometry.py).

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+
+"class BaseGeometry (based on 5-base_geometry.py)."
+
+
+class BaseGeometry():
+    """class BaseGeometry
+    """
+    def area(self):
+        """Mwthod area"""
+        raise Exception("area() is not implemented")
