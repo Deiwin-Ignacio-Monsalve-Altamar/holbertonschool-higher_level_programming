4782680c1fbcc11e012f92bafa0d23ed55ca7675
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-06-02 20:54:31 +0000

    class MyInt that inherits from int:

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,36 @@
 #!/usr/bin/python3
+
+
+"""Rebel Class module"""
+
+
+class MyInt(int):
+    """
+    Arguments:
+            int {[type]} -- [description]
+    """
+
+    def __init__(self, value):
+        """
+        Arguments:
+                value {[type]} -- [description]
+        """
+        self.__value = value
+
+    def __eq__(self, other):
+        """
+        Arguments:
+                other {[type]} -- [description]
+        """
+        return self.__value != other
+
+    def __ne__(self, other):
+        """
+        Arguments:
+                other {[type]} -- [description]
+        """
+        return self.__value == other
+
+    def __str__(self):
+        """Print out the string format"""
+        return "{}".format(self.__value)
