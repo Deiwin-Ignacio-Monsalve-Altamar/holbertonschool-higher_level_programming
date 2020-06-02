b58e3db600592d36eb3e75a292050e8643c383e8
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-06-02 16:08:35 +0000

    function that returns True if the object is an instance of a class that inherited#

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+
+
+"function that returns True if the object is an instance"
+
+
 def inherits_from(obj, a_class):
+    """Return True or False that inherited"""
+    return issubclass(type(obj), a_class)
