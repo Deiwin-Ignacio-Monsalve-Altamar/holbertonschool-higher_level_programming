fb0f0a8b23595c7598f06bfa072c817905cf6139
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-06-02 19:22:57 +0000

    class Rectangle that inherits from BaseGeometry (7-base_geometry.py).

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+
+"""BaseGeometry Class module"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Squeare class
+    Args:
+        Rectangle (class) : class to inherit
+    """
+
+    def __init__(self, size):
+        """Inizialition Square Class"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
