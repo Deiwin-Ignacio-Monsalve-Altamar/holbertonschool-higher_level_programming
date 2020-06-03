4c4c292fcd9525cec73e3c3be5128f609ed782b4
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-06-03 17:05:36 +0000

    function that returns the JSON representation of an object (string):

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
+import json
+
+
+"Fuction convert json to string"
+
+
 def to_json_string(my_obj):
+    """
+    Arguments:
+        my_obj {[type]} -- [description]
+
+    Returns:
+        [json.dupms] -- [convert string]
+    """
+    return json.dumps(my_obj)
