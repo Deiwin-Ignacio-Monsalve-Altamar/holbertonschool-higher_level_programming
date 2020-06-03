3293693adef25e71a6ae3b29193b098c7afb1352
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-06-03 21:23:33 +0000

    class Student that defines a student by:

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+
+
+"""class a Student"""
+
+
+class Student:
+    """Create s student class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Args:
+            first_name ([type]): [description]
+            last_name ([type]): [description]
+            age ([type]): [description]
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Returns:
+            [type]: [description]
+        """
+        return self.__dict__
