2d20de0ebedbfaeaab2ec5bfde0b9afc62c44ac4
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-06-03 16:12:42 +0000

    function that returns the number of lines of a text file:

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
+
+
+"Fuction counter lines in file"
+
+
 def number_of_lines(filename=""):
+    """
+    Keyword Arguments:
+        filename {str} -- [description] (default: {""})
+
+    Returns:
+        int -- counters
+    """
+    with open(filename, encoding='utf-8') as f:
+        for i, letras in enumerate(f):
+            pass
+        return i + 1
