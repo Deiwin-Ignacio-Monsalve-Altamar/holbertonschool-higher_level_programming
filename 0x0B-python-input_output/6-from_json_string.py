e33e4d9ba91ee9bd5dcf9746bfddf69e0e3092ef
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-06-03 17:16:33 +0000

    function that returns an object (Python data structure) represented by a JSON string:

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
+import json
+
+
+"Fuction convert string in Json"
+
+
 def from_json_string(my_str):
+    """
+    Arguments:
+        my_str {[type]} -- [description]
+
+    Returns:
+        [type] -- [description]
+    """
+    return json.loads(my_str)
