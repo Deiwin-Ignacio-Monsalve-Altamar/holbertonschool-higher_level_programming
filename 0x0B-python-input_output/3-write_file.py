37c4046cb6141b28a1cfe48fd18f94c7e79299f1
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-06-03 16:28:27 +0000

    function that writes a string to a text file (UTF8) and returns the number of

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
+
+"Fuction write lines in file"
+
+
 def write_file(filename="", text=""):
+    """
+    Keyword Arguments:
+        filename {str} -- [description] (default: {""})
+        text {str} -- [description] (default: {""})
+
+    Returns:
+        [lines_write] -- [description]
+    """
+    with open(filename, mode='w', encoding="UTF-8") as f:
+        lines_write = f.write("{}".format(text))
+        return lines_write
