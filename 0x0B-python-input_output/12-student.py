85f8617ee9ccf7208de791963b47147f1c36dd38
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-06-03 21:35:11 +0000

    class Student that defines a student by: (based on 11-student.py)

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+
+
+"class function students"
+
+
+class Student:
+    """class studnets
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Args:
+            first_name ([type]): [description]
+            last_name ([type]): [description]
+            age ([type]): [description]
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Args:
+            attrs ([type], optional): [description]. Defaults to None.
+        """
+        if attrs is None:
+            return self.__dict__
+
+        mylist = {}
+
+        for att in attrs:
+            if att in self.__dict__:
+                mylist[att] = self.__dict__[att]
+        return mylist
