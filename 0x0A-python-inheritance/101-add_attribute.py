5b83d769f67a2c1023dfeb9d03e051518e9ed655
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-06-03 02:49:27 +0000

    taks advanced

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,14 +4,14 @@
 "Added an tribute on class"
 
 
-def add(obj, name, value):
+def add_attribute(obj, name, value):
     """
     Arguments:
         obj {[type]} -- [description]
         name {[type]} -- [description]
         value {[type]} -- [description]
     """
-    if not hassattr(obj, "__dict__"):
+    if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
     else:
         setattr(obj, name, value)
