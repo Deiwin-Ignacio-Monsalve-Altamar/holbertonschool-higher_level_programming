0f6178f216de9aac33c770f63a935940805f6953
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-06-03 16:11:43 +0000

    function that reads a text file (UTF8) and prints it to stdout: #1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+
+
+"Fuction read line in file"
+
+
 def read_file(filename=""):
+    """
+    Keyword Arguments:
+        filename {str} -- [description] (default: {""})
+    """
     with open(filename, encoding='utf-8') as f:
         print(f.read(), end="")
