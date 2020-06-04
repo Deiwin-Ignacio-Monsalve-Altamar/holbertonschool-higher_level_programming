6446ecc67659ea37bb75b44469eb600a047227f2
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-06-04 04:58:37 +0000

    tasks advanced

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+
+"""function that writes an Object to a text file, using a JSON representation
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Args
+        filename:
+        search_string:
+        new_string:
+    """
+    aux = ""
+    with open(filename, mode='r', encoding="UTF8") as file:
+        for k in file:
+            if search_string in k:
+                aux += k[:] + new_string
+            else:
+                aux += k[:]
+    with open(filename, mode='w', encoding="UTF8") as file:
+        file.write(aux)
