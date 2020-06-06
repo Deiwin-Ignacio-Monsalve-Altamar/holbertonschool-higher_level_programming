e1ba9ec8bcf5eb497da61210172fc58379fe02dd
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-06-06 03:55:17 +0000

    Update the class Rectangle by updating the public method def update(self, *args): by changing the prototype to update(self, *args, **kwargs) that assigns a key/value argument to attributes:

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,11 +10,11 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """
         Args:
-                width (integer): [width in rectangle]
-                height (integer): [heigth in the rectangle]
-                x (int, optional): [integer asignement]
-                y (int, optional): [integer asignement]
-                id ([type], optional): [hereda de la clase padre].
+            width (integer): [width in rectangle]
+            height (integer): [heigth in the rectangle]
+            x (int, optional): [integer asignement]
+            y (int, optional): [integer asignement]
+            id ([type], optional): [hereda de la clase padre].
         """
         super().__init__(id)
         self.width = width
@@ -31,7 +31,7 @@ class Rectangle(Base):
     def width(self, value):
         """
         Args:
-                value (integer): [integer asignement for width]
+            value (integer): [integer asignement for width]
         """
         if type(value) is not int:
             raise TypeError("width must be an integer")
@@ -43,7 +43,7 @@ class Rectangle(Base):
     def height(self):
         """
         Returns:
-                [int]: [integer asignement for width]
+            [int]: [integer asignement for width]
         """
         return self.__heigth
 
@@ -51,19 +51,19 @@ class Rectangle(Base):
     def height(self, value):
         """
         Args:
-                value (int): [integer asignement for height]
+            value (int): [integer asignement for height]
         """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
-        self.__heigth = value
+        self.__height = value
 
     @property
     def x(self):
         """
         Returns:
-                [int]: [integer asignement for x]
+            [int]: [integer asignement for x]
         """
         return self.__x
 
@@ -71,7 +71,7 @@ class Rectangle(Base):
     def x(self, value):
         """
         Args:
-                value (int): [integer asignement for x]
+            value (int): [integer asignement for x]
         """
         if type(value) is not int:
             raise TypeError("x must be an integer")
@@ -83,7 +83,7 @@ class Rectangle(Base):
     def y(self):
         """
         Returns:
-                [int]: [integer asignement for y]
+           [int]: [integer asignement for y]
         """
         return self.__y
 
@@ -91,7 +91,7 @@ class Rectangle(Base):
     def y(self, value):
         """
         Args:
-                value (y): [integer asignement for y]
+        value (y): [integer asignement for y]
         """
         if type(value) is not int:
             raise TypeError("y must be an integer")
@@ -99,7 +99,7 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         self.__y = value
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """args: argument in tuple
         """
         count = 1
@@ -110,33 +110,45 @@ class Rectangle(Base):
                 elif count == 2:
                     self.__width = value
                 elif count == 3:
-                    self.__heigth = value
+                    self.__height = value
                 elif count == 4:
                     self.__x = value
                 elif count == 5:
                     self.__y = value
                 count += 1
+        elif len(kwargs):
+            for item, value in kwargs.items():
+                if item == 'id':
+                    self.id = value
+                elif item == 'width':
+                    self.__width = value
+                elif item == 'height':
+                    self.__height = value
+                elif item == 'x':
+                    self.__x = value
+                elif item == 'y':
+                    self.__y = value
 
     def area(self):
         """
         Returns:
-                area: return value of the area
+        area: return value of the area
         """
-        return self.__heigth * self.__width
+        return self.__height * self.__width
 
     def display(self):
         """ function self print #
         """
         for i in range(self.__y):
             print()
-        for j in range(self.__heigth):
+        for j in range(self.__height):
             print(self.__x * " " + self.__width * "#")
 
     def __str__(self):
         """
         Returns:
-                [type]: [description]
+                        [type]: [description]
         """
         return '[Rectangle] ({}) {}/{} - '\
             '{}/{}'.format(self.id, self.__x, self.__y,
-                           self.__width, self.__heigth)
+                           self.__width, self.__height)
