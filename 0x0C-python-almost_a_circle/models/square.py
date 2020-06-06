e1c1c8b2d268744240706a64bb953ed8f8ccd5e6
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-06-06 21:34:36 +0000

    Update the class Square by adding the public method def update(self, *args, **kwargs) that assigns attributes:

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -33,6 +33,35 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
+    def update(self, *args, **kwargs):
+        """Updates the square instance
+        Args:
+            args (list): argument lists
+            kwargs (dict): argument dictionary
+        """
+        if len(args):
+            count = 1
+            for value in args:
+                if count == 1:
+                    self.id = value
+                if count == 2:
+                    self.size = value
+                if count == 3:
+                    self.x = value
+                if count == 4:
+                    self.y = value
+                count += 1
+        elif len(kwargs):
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            if 'size' in kwargs:
+                self.size = kwargs['size']
+            if 'x' in kwargs:
+                self.x = kwargs['x']
+            if 'y' in kwargs:
+                self.y = kwargs['y']
+
+
     def __str__(self):
         return '[Square] ({}) {}/{} - {}'.format(self.id,
                                                  self.x, self.y, self.width)
