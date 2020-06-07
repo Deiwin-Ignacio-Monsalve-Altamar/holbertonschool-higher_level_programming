9fe7fb00c980b28057ebf9a07536308961a11e06
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-06-07 20:56:33 +0000

    Update the class Base by adding the class method def load_from_file(cls): that returns a list of instances:

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,8 +16,8 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+            type(self).__nb_objects += 1
+            self.id = type(self).__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
@@ -29,21 +29,22 @@ class Base:
             [type]: [description]
         """
         if list_dictionaries is None or list_dictionaries is []:
-            return "[]"
+            return []
         return json.dumps(list_dictionaries)
 
+    @classmethod
     def save_to_file(cls, list_objs):
-        """
+        """Create objects from object instances, then save to json file
         Args:
-            list_objs ([type]): [description]
+            cls (class): the class of the object instances
+            list_objs (list): list of class objects
         """
-        filename = cls.__name__ + ".json"
-        with open(filename, mode='w', encoding="UTF-8") as f:
+        with open(cls.__name__ + '.json', mode='w', encoding='utf-8') as f:
             if list_objs is None:
                 f.write("[]")
             else:
-                objs = [cls.to_dictionary(obj) for obj in list_dictionaries]
-                f.write(cls-to_json_string)
+                objs = [cls.to_dictionary(obj) for obj in list_objs]
+                f.write(cls.to_json_string(objs))
 
     @staticmethod
     def from_json_string(json_string):
@@ -55,5 +56,36 @@ class Base:
             [type]: [description]
         """
         if json_string is None or json_string is []:
-            return "[]"
+            return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        Returns:
+            [type]: [description]
+        """
+        if cls.__name__ == 'Rectangle':
+            new_clase = cls(1, 1)
+        elif cls.__name__ == 'Square':
+            new_clase = cls(1)
+        new_clase.update(**dictionary)
+        return new_clase
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        Returns:
+            [type]: [description]
+        """
+        filename = cls.__name__ + ".json"
+        list_new = []
+        with open(filename, mode="r", encoding="UTF-8") as f:
+            json_string = f.read()
+            new_string = cls.from_json_string(json_string)
+            for item in new_string:
+                if isinstance(item, dict) is True:
+                    list_new.append(cls.create(**item))
+                else:
+                    list_new.append(item)
+        return list_new
