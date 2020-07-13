ef0f18c5fcb9e707add93a1ab3a5bea14adc9782
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-07-13 23:10:41 +0000

    script that converts hbtn_0c_0 database to UTF8

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,4 @@
 -- Import in hbtn_0c_0 database this table
-SELECT city, AVG(values) AS avg_temp FROM temperatures WHERE month=7 or month=8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures
+WHERE month = 7 OR month = 8
+GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
