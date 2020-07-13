1287b502873354dc38ccacd16de6170d9c03f94e
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-07-13 20:07:18 +0000

    tasks advanced

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 --script que enumere la cantidad de registros con la misma puntuación en la tabla
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score DESC;
+SELECT score, count(*) AS number FROM second_table GROUP BY score DESC;
