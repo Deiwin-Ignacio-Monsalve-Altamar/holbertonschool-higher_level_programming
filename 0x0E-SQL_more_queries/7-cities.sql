b4f7c7fca26e0d72a59dd48e3afaef6e51f81bc5
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-07-14 19:12:05 +0000

    script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,8 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
-(id INT UNIQUE AUTO_INCREMENT NOT NULL,
-state_id INT NOT NULL,
-PRIMARY KEY(id),
-FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.state(id)
+
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+    id INT UNIQUE AUTO_INCREMENT NOT NULL,
+    state_id INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.state(id)
 );
