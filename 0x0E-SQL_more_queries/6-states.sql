74d3b6b5c82b0b18a3446a46e3ece9b28379c13e
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-07-14 18:50:32 +0000

    script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- script that creates the database
+CREATE DATABASE hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(id INT NOT NULL UNIQUE AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
