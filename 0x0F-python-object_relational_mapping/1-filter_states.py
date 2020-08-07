77a2dc2fc749fa0d7ab93bfb173a1f2524da4d0b
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-07 12:38:07 -0500

    ript that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+
+def filterStates():
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+
+    cur.execute('SELECT * FROM states WHERE name LIKE "N%"')
+    dataRow = cur.fetchall()
+    for row in dataRow:
+        print(row)
+
+    db.close()
+    cur.close()
+
+
+if __name__ == '__main__':
+    filterStates()
