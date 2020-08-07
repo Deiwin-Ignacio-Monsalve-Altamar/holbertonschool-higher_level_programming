abc95e53a625dec95d7580664201f8e702a1567b
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-07 17:45:52 -0500

    write a script that takes in arguments and displays all values in the states table of

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+
+def inyeccionSQL():
+    db = MySQLdb.connect(host='localhost', port=3303, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states \
+                WHERE name=%s \
+                ORDER BY id ASC", (sys.argv[4],))
+    dataRow = cur.fetchall()
+    for row in dataRow:
+        print(row)
+
+    db.close()
+    cur.close()
+
+
+if __name__ == '__main__':
+    inyeccionSQL()
