e05aea07dcaf576993d14e915024f9c19a4bdcfd
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-08 08:08:11 -0500

    update diles tasks 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""do you remember the previous task? 
+"""
 import MySQLdb
 import sys
 
 
 def inyeccionSQL():
+    """you test "Arizona'
+    """
     db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
@@ -12,7 +16,8 @@ def inyeccionSQL():
                 WHERE name=%s \
                 ORDER BY id ASC", (sys.argv[4],))
     for row in cur.fetchall():
-        print(row)
+        if row[1] == sys.argv[4]:
+            print(row)
 
     db.close()
     cur.close()
