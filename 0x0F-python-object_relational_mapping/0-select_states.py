7a14637ce60e9b87fd4b9be641a1963f831fce45
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-08 07:53:46 -0500

    update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
+"""Exercise
+"""
+import MySQLdb
+import sys
 
 
 def getallstates():
-    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
+    """COnect
+    """
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
 
     cur.execute('SELECT * FROM states ORDER BY id ASC')
-    dataRow = cur.fetchall()
-    for row in dataRow:
+    for row in cur.fetchall():
         print(row)
 
     cur.close()
@@ -16,6 +21,4 @@ def getallstates():
 
 
 if __name__ == '__main__':
-    import MySQLdb
-    import sys
     getallstates()
