cea13f39d93703be9fa1594d623a86a5e8b5c0f3
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-08 08:01:57 -0500

    update diles tasks 2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""[lists all states with a name starting with N]
+"""
 import MySQLdb
 import sys
 
 
 def filterStates():
+    """name starting with N (upper N)
+    """
     db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
