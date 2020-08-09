a41474b370e19268b847243280974b4fc82ac260
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-08 20:18:43 -0500

    script that adds the State object “Louisiana” to the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""script that adds the State
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+def getState():
+    """object Louisiana to the database
+    """
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_states = State(name='Louisiana')
+    session.add(new_states)
+    
+    for state in session.query(State).order_by(State.id).all():
+        if state.name == "Louisiana":
+            print("{}".format(state.id))
+
+    session.commit()
+    session.close()
+
+
+if __name__ == '__main__':
+    getState()
