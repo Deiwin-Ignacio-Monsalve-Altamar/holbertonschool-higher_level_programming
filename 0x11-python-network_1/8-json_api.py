26ba3c8abb4ad34e681b34c6537f7a9a8828a190
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-24 16:18:22 -0500

    script that takes in a letter and sends a POST request to

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""script that takes in a letter"""
+import requests
+import sys
+
+
+def search_api(url, q):
+    """script that takes in a letter"""
+    data = {'q': q}
+    r = requests.post(url, data=data)
+
+    try:
+        n_dict = r.json()
+        if n_dict:
+            print("[{}] {}".format(n_dict['id'], n_dict['name']))
+        else:
+            print("No result")
+    except Exception:
+        print("Not valide JSON")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        q = sys.argv[1]
+    else:
+        q = ""
+    url = "http://0.0.0.0:5000/search_user"
+    search_api(url, q)
