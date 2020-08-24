3e28af030ddaf0740a4d71d5ca5c3d3a9876451a
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-24 15:00:13 -0500

    that takes in a URL, sends a request to the URL and displays the body of the response.

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""sends a request to the URL"""
+import requests
+import sys
+
+
+def errorCode(url):
+    """sends a request to the URL"""
+    r = requests.get(url)
+    if (r.status_code == 200):
+        print(r.text)
+    else:
+        print("Error code: {}".format(r.status_code))
+
+
+if __name__ == '__main__':
+    errorCode(sys.argv[1])
