41cc38c66803b52db844cace956e2743c77ff526
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-24 14:29:51 -0500

    that takes in a URL and an email address, sends a POST request to the passed URL

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""takes in a URL and an email address, sends a POST"""
+import requests
+import sys
+
+
+def postEmail(url, email):
+    """takes in a URL and an email address, sends a POST"""
+    data = {'email': email}
+    r = requests.post(url, data=data)
+    print("{}".format(r.text))
+
+
+if __name__ == '__main__':
+    postEmail(sys.argv[1], sys.argv[2])
