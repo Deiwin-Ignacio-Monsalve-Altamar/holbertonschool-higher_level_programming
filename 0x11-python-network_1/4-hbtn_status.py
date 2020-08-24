3bcaebd78bcb440d6db57a6e01ee8345eb897c9f
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-24 14:04:23 -0500

    Python script that fetches https://intranet.hbtn.io/status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Python script that fetches"""
+import requests
+
+
+def statusPython():
+    """Python script that fetches"""
+    r = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
+
+
+if __name__ == '__main__':
+    statusPython()
