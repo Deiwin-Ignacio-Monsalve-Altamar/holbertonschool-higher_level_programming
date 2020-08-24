cb7bb98472ad4a053779e7127f76bae5ebe3bc9c
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-24 12:57:22 -0500

    Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""that takes in a URL and an email"""
+import urllib.parse
+import urllib.request
+from sys import argv
+
+
+def postEmail(args, email):
+    """
+    Args:
+        args (): [request url]
+        email (): [post value]
+    """
+    value = {'email': email}
+    data = urllib.parse.urlencode(value)
+    data = data.encode('ascii')
+    req = urllib.request.Request(args, data)
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print("{}".format(html.decode('utf-8')))
+
+
+if __name__ == '__main__':
+    postEmail(argv[1], argv[2])
