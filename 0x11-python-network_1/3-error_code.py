146a63ae93498b64e7bb55155de7d614a34d8fe2
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-24 13:15:06 -0500

    Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8).

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""sends a request to the URL"""
+import urllib.request
+import sys
+
+
+def desplayError(url):
+    """
+    Args:
+        url ([type]): [description]
+    """
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            html = response.read()
+            print("{}".format(html.decode("utf-8")))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
+
+
+if __name__ == '__main__':
+    desplayError(sys.argv[1])
