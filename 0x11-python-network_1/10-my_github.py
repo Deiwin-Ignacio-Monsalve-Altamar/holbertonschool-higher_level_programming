197c98f69df22493a3df763b75c8f519e4115c28
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-24 16:38:23 -0500

    script that takes your Github credentials (username and password) and

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""takes your Github credential"""
+import requests
+import sys
+
+
+def my_github(url, users, password):
+    """takes your Github credential"""
+    
+    r = requests.get(url, auth=(users, password))
+    dict_json = r.json()
+    print(dict_json.get('id'))
+
+
+if __name__ == '__main__':
+    url = 'https://api.github.com/user'
+    users = sys.argv[1]
+    password = sys.argv[2]
+    my_github(url, users, password)
