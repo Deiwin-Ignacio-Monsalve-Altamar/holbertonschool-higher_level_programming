1f96ba094f2d06dced824b24aa57c5f8120ccbb0
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-24 12:23:47 -0500

    sends a request to the URL and displays the value of the X-Request-Id

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""sends a request to the URL"""
+import urllib.request
+import sys
+
+
+def responseHeader(arg):
+    """
+    Args:
+        arg (argument): [url sends]
+    """
+    with urllib.request.urlopen(arg) as response:
+        dictResponse = response.info()
+        print("{}".format(dictResponse['X-Request-Id']))
+
+
+if __name__ == '__main__':
+    responseHeader(sys.argv[1])
