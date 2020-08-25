ed9308772bd7df49300a55b18d6be09c7622528b
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-24 19:09:17 -0500

    staff evaluates candidates applying for a back-end position with multiple technical challenges,

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""evaluates candidates applying for a back-end position"""
+import requests
+import sys
+
+
+def time_for_interviem(owner, repo):
+    """script that takes 2 arguments in order"""
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+    r = requests.get(url)
+
+    dict_n = r.json()
+    count = 0
+    for x in dict_n:
+        print("{}: {}".format(x.get('sha'), x.get(
+            'commit').get('author').get('name')))
+        count += 1
+        if count == 10:
+            break
+
+
+if __name__ == '__main__':
+    owner = sys.argv[2]
+    repo = sys.argv[1]
+    time_for_interviem(owner, repo)
