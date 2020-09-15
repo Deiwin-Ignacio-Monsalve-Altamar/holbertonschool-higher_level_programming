b81a7c47cd20a4b3bf5da09a21e4ff086eca0dde
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-09-15 14:15:38 -0500

    this script by adding a new function incr that increments the integer value.

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function () { this.value += 1; };
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
