62635d85814778de02adcf2c312661e83ddaca4e
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-09-15 13:09:11 -0500

    script that computes and prints a factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+const args = process.argv;
+
+if (args[2] === undefined) {
+  console.log('1');
+} else {
+  const result = factorial(parseInt(args[2]));
+  console.log(result);
+}
+
+function factorial (num) {
+  if (num === 1) {
+    return num;
+  }
+  return num * factorial(num - 1);
+}
