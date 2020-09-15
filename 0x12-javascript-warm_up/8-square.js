2b9d8ab17a0ca8c72874b7bef56ffbe8d6c630d3
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-09-15 12:55:10 -0500

    script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+
+if (process.argv[2] === undefined | isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  for (let index = 1; index <= process.argv[2]; index++) {
+    console.log('X'.repeat(process.argv[2]));
+  }
+}
