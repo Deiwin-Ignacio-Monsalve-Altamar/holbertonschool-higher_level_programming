c55216198e1eda61745e585d0a880ff7761e7e67
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-09-15 12:49:06 -0500

    script that prints x times “C is fun”

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+const len = process.argv;
+if (process.argv[2] === undefined) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let index = 0; index < len[2]; index++) {
+    console.log('C is fun');
+  }
+}
