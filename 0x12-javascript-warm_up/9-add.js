1005458a992006b9c295a12768ed4b594ab57306
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-09-15 13:02:39 -0500

    script that prints the addition of 2 integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+const fisrtNumber = process.argv[2];
+const secondNumber = process.argv[3];
+if (isNaN(fisrtNumber) || isNaN(fisrtNumber)) {
+  console.log('NaN');
+} else {
+  console.log(add(parseInt(fisrtNumber), parseInt(secondNumber)));
+}
+
+function add (a, b) {
+  return a + b;
+}
