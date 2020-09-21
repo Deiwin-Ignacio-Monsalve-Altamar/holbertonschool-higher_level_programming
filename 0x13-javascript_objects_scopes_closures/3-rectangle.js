41c5b8a378dfaca73361230b96aba1ac22c0d03f
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-09-21 10:55:12 -0500

    class Rectangle that defines a rectangle: with print

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+/* class Rectangle that defines a rectangle with print */
+
+class Rectangle {
+  constructor (w = 0, h = 0) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let index = 0; index < this.height; index++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
