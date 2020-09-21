7a3a8a24189d70aa65a7c85150128bc1292aad75
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-09-21 12:10:53 -0500

    script that imports an array and computes a new array.

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const mapped = require('./100-data').list;
+
+function print (list) {
+  console.log(list);
+
+  const mapeado = list.map((val, idx) => idx * val);
+  console.log(mapeado);
+}
+
+print(mapped);
