58cdff8a9e40935572f2e41f22be91a38bf42579
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-09-21 11:05:14 -0500

    class Rectangle that defines a rectangle: rotate double

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,30 @@
 #!/usr/bin/node
+/* class Rectangle rectangle rotate duoble */
+
+class Rectangle {
+  constructor (w = 0, h = 0) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let index = 0; index < this.height; index++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = Rectangle;
