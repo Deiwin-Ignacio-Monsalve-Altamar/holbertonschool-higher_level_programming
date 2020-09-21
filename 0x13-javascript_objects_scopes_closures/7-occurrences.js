15f84b2596daee79622eeabbe7c975303f62b765
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-09-21 11:37:41 -0500

    function that returns the number of occurrences in a list:

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+/* that returns the number of occurrences in a list: */
+
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (let index = 0; index < list.length; index++) {
+    if (list[index] === searchElement) {
+      count++;
+    }
+  }
+  return count;
+};
