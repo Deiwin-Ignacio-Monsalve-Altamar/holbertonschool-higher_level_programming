c519c94dfb340045e15683851bb47c0e5c42f43d
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-09-21 11:15:44 -0500

    class Square that defines a rectangle: rotate double

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,1 +1,36 @@
 #!/usr/bin/node
+/* defines a square and inherits from Rectangle */
+
+class Rectangle {
+  constructor (w = 0, h = 0) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let index = 0; index < this.height; index++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+
+module.exports = Square;
