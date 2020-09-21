758a8d15d6f562161fd684887084892a886f885c
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-09-21 11:44:17 -0500

    that returns the reversed version of a list:

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+/* that returns the reversed version of a list */
+
+exports.esrever = function (list) {
+  const list_reverse = [];
+  while (list.length) {
+    list_reverse.push(list.pop());
+  }
+  return list_reverse;
+};
