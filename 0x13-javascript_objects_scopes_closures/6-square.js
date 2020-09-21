76be9326726758f9ede5497dc29aade4f4a19250
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-09-21 11:32:14 -0500

    class Square that defines a rectangle: rotate double

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+/* Square that defines a square and inherits from Square of 5-square.js */
+
+const Rectangulo = require('./5-square');
+class Square extends Rectangulo {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let index = 0; index < this.height; index++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
