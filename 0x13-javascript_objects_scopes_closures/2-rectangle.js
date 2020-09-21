cb31d4e72e2443cdfff8b0313d41839c3a42d775
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-09-21 10:24:03 -0500

    class Rectangle that defines a rectangle with value

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+class Rectangle {
+  constructor (w = 0, h = 0) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = w;
+    }
+  }
+}
+
+module.exports = Rectangle;
