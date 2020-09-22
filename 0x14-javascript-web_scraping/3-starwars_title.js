6f4ce4116f5bd8ebce40971d6f88bed987f4cf86
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-09-22 15:07:34 -0500

    script that prints the title of a Star Wars

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+/* script that prints the title of a Star Wars */
+
+const request = require('request');
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    console.log(JSON.parse(body).title);
+  }
+});
