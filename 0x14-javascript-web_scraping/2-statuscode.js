ea38ffcb7dc90c33ef89d001dd4bdc7357df0cd9
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-09-22 14:50:09 -0500

    script that display the status code of a GET request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+/* script that display the status code of a GET request */
+const request = require('request');
+request(process.argv[2], (error, response) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log(`Code: ${response.statusCode}`);
+});
