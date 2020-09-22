8ca20aa7bbe12e206bb5cdea53f6cdf70c8635b2
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-09-22 14:30:24 -0500

    script that writes a string to a file.

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+/* script that writes a string to a file. */
+
+const fs = require('fs');
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err) {
+  if (err) {
+    return console.log(err);
+  }
+});
