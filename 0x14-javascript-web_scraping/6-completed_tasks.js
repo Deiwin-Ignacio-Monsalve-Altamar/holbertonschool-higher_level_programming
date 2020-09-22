c2931d258d51e0fa7eb36adf87d56ce1d4b2ec67
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-09-22 16:35:59 -0500

    Write a script that computes the number of tasks completed by user id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+/* script that computes the number of tasks completed by user id */
+const request = require('request');
+request(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    const array = JSON.parse(body);
+    const obj = {};
+    for (let index = 0; index < array.length; index++) {
+      if (array[index].completed) {
+        if (!(array[index].userId in obj)) {
+          obj[array[index].userId] = 0;
+        }
+        obj[array[index].userId] += 1;
+      }
+    }
+    console.log(obj);
+  }
+});
