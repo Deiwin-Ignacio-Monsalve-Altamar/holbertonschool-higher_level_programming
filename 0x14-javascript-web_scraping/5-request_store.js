9f034d72ef1f584fd0346e308beb19b0b1e52930
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-09-22 15:50:01 -0500

    that gets the contents of a webpage and stores it in a file.

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,12 +5,12 @@ request(process.argv[2], (error, response, body) => {
   if (error) {
     console.log(error);
   } else if (response.statusCode === 200) {
-    let contents = body;
-    let fs = require('fs');
-    fs.writeFile(process.argv[3], contents, 'utf-8', (err) =>{
-        if(err){
-            console.log(err)
-        }
-    })    
-}
+    const contents = body;
+    const fs = require('fs');
+    fs.writeFile(process.argv[3], contents, 'utf-8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
 });
