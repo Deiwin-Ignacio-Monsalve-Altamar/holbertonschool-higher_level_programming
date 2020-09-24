4179bd6e27fe3109ea5032566b456a53974c170e
Deiwin Monsalve <deiwin1987@gmail.com>
2020-09-24 15:33:05 -0500

    fetches and replaces the name of this URL

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,1 +1,5 @@
 #!/usr/bin/node
+/*  fetches and replaces the name of this URL */
+$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data) {
+  $('DIV#character').text(data.name);
+});
