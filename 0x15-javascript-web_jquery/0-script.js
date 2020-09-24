3fd54dae12a59ef74bcca0dc063c91ca8566073d
Deiwin Monsalve <deiwin1987@gmail.com>
2020-09-24 13:13:09 -0500

    script that updates the text color of the HTML tag HEADER to red (#FF0000):

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-/*script that updates the text color of the HTML tag HEADER to red (#FF0000)*/
+/* script that updates the text color of the HTML tag HEADER to red (#FF0000) */
 
-let element = document.querySelector('header');
-element.style.color = "#FF0000";
+const element = document.querySelector('header');
+element.style.color = '#FF0000';
