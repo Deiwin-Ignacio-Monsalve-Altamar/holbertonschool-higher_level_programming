fccf536032c180bb3777e5b90ea4a4a458ec347f
Deiwin Monsalve <deiwin1987@gmail.com>
2020-09-24 16:05:00 -0500

    script that fetches from https://fourtonfish.com/hellosalut/?lang=fr and displays the value of hello from that fetch in the HTML’s tag DIV#hello.

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,1 +1,6 @@
 #!/usr/bin/node
+/* script that fetches from https://fourtonfish.com/hellosalut/?lang=fr */
+const URL = 'https://fourtonfish.com/hellosalut/?lang=fr';
+$.get(URL, function (data) {
+  $('DIV#hello').text(data.hello);
+});
