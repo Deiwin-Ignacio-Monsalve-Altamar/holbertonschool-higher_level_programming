9d1b8a9d82587918f5d3323f25f494c3ab8a90c6
Deiwin Monsalve <deiwin1987@gmail.com>
2020-09-24 15:47:28 -0500

    cript that fetches and lists all movies title by using this URL

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+/* script that fetches and lists all movies title by using this URL */
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+  const films = data.results;
+  for (let i = 0; i < films.length; ++i) {
+    $('#list_movies').append(`<li>${films[i].title}</li>`);
+  }
+});
